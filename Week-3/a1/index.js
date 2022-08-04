50ba5b0bcd2401267f7537e0039b912f74333a36
mr-hungama <mihir.rego@hungama.com>
2022-08-04 21:29:15 +0530

    Update index.js
    finished memoize functionality

--- Week-3/a1/index.js
+++ Week-3/a1/index.js
@@ -1,12 +1,41 @@
+function memoize(fn) {
+    const cache = new Map();
+    return function(...args) {
+        var sum = 0;
+        const key = args.toString();
+        console.log('key =', key);
+        if(cache.has(key)) {
+            sum = cache.get(key);
+            console.log("found in cache, sum = ", sum);
+            return sum;
+        }
+
+        sum = fn(...args);
+        cache.set(key, sum);
+        console.log("adding to cache, sum =", sum);
+        return sum;
+    }
+}
+
 function add(...args) {
-    console.log(args);
     var sum = 0;
     for (let val in args) {
         sum += args[val];
-    }
-    console.log('sum =', sum);    
+    }  
     return sum;
 }
 
-add(100, 200, 300, 400);
-add(400, 300, 200, 100);
+function time(fn) {
+    console.time();
+    fn();
+    console.timeEnd();
+}
+
+const memAdd = memoize(add);
+
+memAdd(100, 200, 300, 400);
+memAdd(100, 200, 300, 400);
+memAdd(100, 200, 300, 400);
+memAdd(100, 200, 300, 400);
+memAdd(300, 200, 300, 400);
+memAdd(300, 200, 300, 400);
