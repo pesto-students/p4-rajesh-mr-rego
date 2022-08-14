36d039cf982385c6647a9c9c5e93b174b6272ea3
mr-hungama <mihir.rego@hungama.com>
2022-08-14 08:58:18 +0530

    Update index-4.2.js
    Completed assignment

--- Week-4/4.2/index-4.2.js
+++ Week-4/4.2/index-4.2.js
@@ -1,24 +1,48 @@
-class Person {
-    constructor(name, age) {
-        this.name = name;
-        this.age = age;
-    }
+function Person(name, age) {
+    this.name = name;
+    this.age = age;
+}
+
+Person.prototype.printDetails = function() {
+    console.log(`This person is ${this.name} who is ${this.age} years old.`);
+}
+
+function Teacher(name, age) {
+    Person.call(this, name, age);
+}
+
+Teacher.prototype.teach = function(subject) {
+    this.subject = subject;
+    console.log(`${this.name} is now teaching ${this.subject}.`);
+}
+
+Object.setPrototypeOf(Teacher.prototype, Person.prototype);
+
+const t = new Teacher('Mihir Rego', 40);
+t.printDetails();
+t.teach('Physics');
+
+// class Person {
+//     constructor(name, age) {
+//         this.name = name;
+//         this.age = age;
+//     }
  
-    printDetails() {
-        console.log(`This person is ${this.name} who is ${this.age} years old`);
-    }
-}
+//     printDetails() {
+//         console.log(`This person is ${this.name} who is ${this.age} years old.`);
+//     }
+// }
 
-class Teacher extends Person {
-    constructor(name, age) {
-        super(name, age);
-    }
+// class Teacher extends Person {
+//     constructor(name, age) {
+//         super(name, age);
+//     }
 
-    teach(subject) {
-        this.subject = subject;
-        console.log(`${this.name} is now teaching ${this.subject}`);
-    }
-}
+//     teach(subject) {
+//         this.subject = subject;
+//         console.log(`${this.name} is now teaching ${this.subject}.`);
+//     }
+// }
 
-const t = new Teacher('Mihir', 40);
-t.teach('Mathematics');
+// const t = new Teacher('Mihir', 40);
+// t.teach('Mathematics');
