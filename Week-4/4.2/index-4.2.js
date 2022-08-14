f351a405ce0549aa36349835e617e5562b9038d4
mr-hungama <mihir.rego@hungama.com>
2022-08-14 08:43:17 +0530

    Update index-4.2.js
    Added Person class

---
+++ Week-4/4.2/index-4.2.js
@@ -0,0 +1,10 @@
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+ 
+    printDetails() {
+        console.log(`This person is ${this.name} who is ${this.age} years old`);
+    }
+}
