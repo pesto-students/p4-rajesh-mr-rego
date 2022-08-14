8b10087db30e569ec8be0f448fd4495a6fc834bd
mr-hungama <mihir.rego@hungama.com>
2022-08-14 08:53:05 +0530

    Update
    Implemented with class

--- Week-4/4.2/index-4.2.js
+++ Week-4/4.2/index-4.2.js
@@ -8,3 +8,17 @@ class Person {
         console.log(`This person is ${this.name} who is ${this.age} years old`);
     }
 }
+
+class Teacher extends Person {
+    constructor(name, age) {
+        super(name, age);
+    }
+
+    teach(subject) {
+        this.subject = subject;
+        console.log(`${this.name} is now teaching ${this.subject}`);
+    }
+}
+
+const t = new Teacher('Mihir', 40);
+t.teach('Mathematics');
