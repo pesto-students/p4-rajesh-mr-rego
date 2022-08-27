0c9ddfd716ee702d7916d2faad7e3b345259f8c5
mr-hungama <mihir.rego@hungama.com>
2022-08-27 16:07:14 +0530

    updated assignment
    fixed the cyclic error

--- Week-6/6.2/assignment6.2.js
+++ Week-6/6.2/assignment6.2.js
@@ -1,87 +1,76 @@
+// constants for cursor traversal, we always start with Direction R
 const DIR_R = 'r';
 const DIR_D = 'd';
 const DIR_L = 'l';
 const DIR_U = 'u';
 
 function spiralIt(arr) {
-    console.log(arr);
-    // console.log(arr[0][0]);
-
+    // initialise new array for spiral element addition
     let newArr = [];
 
     const total_elements = arr.length * arr[0].length;
 
     let dir = DIR_R;    
 
-    let rowStart = 1;
-    let rowEnd   = arr[0].length - 1;
-    let row = 0;
+    // initialise first limits
+    let rowStart = 0;
+    let rowEnd   = arr.length - 1;
+    let row = rowStart;
 
     let colStart = 0;
-    let colEnd   = arr.length - 1;
+    let colEnd   = arr[0].length - 1;
     let col = colStart;
 
-    // console.log(total_elements, dir);
-
+    // I tried with a while loop, but was getting undefined errors so just used a 'for'
     for(let el=0 ; el<total_elements ; el++) {
         let val = arr[row][col];
-        // console.log('--------------------------------')
-        // console.log('dir =', dir, 'val = ', val);
-        // console.log('rowStart =', rowStart, 'rowEnd = ', rowEnd, 'row =', row);
-        // console.log('colStart =', colStart, 'colEnd = ', colEnd, 'col =', col);
-
         if(val !== undefined) newArr.push(val);
 
         switch(dir.toLowerCase()) {
+            // columns are traversed left to right, and when end is reached, row start is updated for down movement
             case DIR_R:
                 if(col < colEnd) {
                     col++;
                 } else {
                     dir = DIR_D;
                     
-                    // rowStart++;
+                    rowStart++;
                     row = rowStart;
                 }
             break;
             
+            // columns are traversed up to down, and when end is reached, column start is updated for left movement
             case DIR_D:
                 if(row < rowEnd) {
                     row++;
                 } else {
                     dir = DIR_L;
 
-                    let temp = colStart;
-                    colStart = colEnd - 1;
-                    colEnd = temp;
-
-                    col = colStart;
+                    colEnd--;
+                    col = colEnd;
                 }
             break;
-
+            
+            // columns are traversed right to left, and when end is reached, row start is updated for up movement
             case DIR_L:
-                if(col > colEnd) {
+                if(col > colStart) {
                     col--;
                 } else {
                     dir = DIR_U;
                     
-                    let temp = rowStart;
-                    rowStart = rowEnd - 1;
-                    rowEnd = temp;
-
-                    row = rowStart;
+                    rowEnd--;
+                    row = rowEnd
                 }
             break;
-
+            
+            // columns are traversed down to up, and when end is reached, row start is updated for right movement
             case DIR_U:
-                if(row > rowEnd) {
+                if(row > rowStart) {
                     row--;
                 } else {
                     dir = DIR_R;
                     
-                    let temp = colStart;
-                    colStart = colEnd + 1;
-                    colEnd = temp;
-
+                    colStart++;
                     col = colStart;
                 }
             break;
@@ -111,10 +100,28 @@ const input5x5 = [
     [ 16,  17,  18,  19, 20],
     [ 21,  22,  23,  24, 25],
 ];
-// console.log(input);
-// console.log('---------------------------------------------');
-// spiralIt(input3x3);
-// spiralIt(input4x4);
-const arr = spiralIt(input5x5);
-console.log(arr);
 
+const input2x5 = [
+    [  1,  2,  3,  4,  5],
+    [  6,  7,  8,  9, 10]
+];
+
+const input5x2 = [
+    [  1,  2],
+    [  3,  4],
+    [  5,  6],
+    [  7,  8],
+    [  9, 10],
+];
+
+const input1x5 = [
+    [1],
+    [2],
+    [3],
+    [4],
+    [5]
+];
+
+
+const arr = spiralIt(input1x5);
+console.log(arr);
