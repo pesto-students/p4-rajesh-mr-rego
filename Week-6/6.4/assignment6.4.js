4aedd64b8ce3cb045b9ced336e1965f95dbc90e0
mr-hungama <mihir.rego@hungama.com>
2022-08-27 16:59:15 +0530

    Update assignment
    added new advanced functionality that returns the buy and sell days

--- Week-6/6.4/assignment6.4.js
+++ Week-6/6.4/assignment6.4.js
@@ -1,4 +1,4 @@
-function tradeStock(arr, quantity) {
+function returnMaxProfit(arr, quantity) {
     let mainArr = arr;
     let arrLen = mainArr.length;
     // console.log(arrLen);
@@ -25,14 +25,44 @@ function tradeStock(arr, quantity) {
         arrLen = mainArr.length;
     }
 
-    console.log('max profit =', maxProfit);
     return maxProfit;
 }
 
-let stockA = [7, 1, 5, 3, 6, 4];
-let stockB = [7, 6, 5, 4, 3, 2];
-let stockC = [3, 1, 8, 9, 1, 5];
-let stockD = [1, 2, 3, 4, 5, 6];
-let quantity = 1;
+function tradeStockAdvanced(arr, quantity) {
+    let maxProfit = 0;
+    let buyDay = 0;
+    let sellDay = 0;
 
-tradeStock(stockA, quantity);
+    for(let bday=0 ; bday<arr.length-1 ; bday++) {
+        for(let sday=bday+1 ; sday<arr.length ; sday++) {
+            // console.log('buy day =', arr[bday], 'sell day =', arr[sday]);
+
+            let currProfit = quantity * (arr[sday] - arr[bday]);
+            if(currProfit >= maxProfit) {
+                maxProfit = currProfit;
+                buyDay = bday + 1;
+                sellDay = sday + 1;
+            }
+        }
+
+    }
+
+    return [maxProfit, buyDay, sellDay];
+}
+
+
+const stockA = [7, 1, 5, 3, 6, 4];
+const stockB = [7, 6, 5, 4, 3, 2];
+const quantity1 = 1;
+
+const profit = returnMaxProfit(stockA, quantity1);
+console.log('Maximum profit possible is Rs.', profit, 'for', quantity1, 'stock/s.');
+
+console.log('-----------------------------------------');
+const stockC = [3, 1, 8, 9, 1, 5];
+const stockD = [1, 2, 3, 4, 5, 6];
+const quantity2 = 1;
+
+const newProfit = tradeStockAdvanced(stockC, quantity2);
+console.log(`Maximum profit of Rs. ${newProfit[0]} can be earned by buying on day ${newProfit[1]} and selling on day ${newProfit[2]}.`);
+
