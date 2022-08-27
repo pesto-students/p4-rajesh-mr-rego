4f4665ba7626182e6f535f3766a4a8f16d67a650
mr-hungama <mihir.rego@hungama.com>
2022-08-27 10:07:06 +0530

    update assignment 6.1
    added comments

--- Week-6/6.1/assignment6.1.js
+++ Week-6/6.1/assignment6.1.js
@@ -1,11 +1,14 @@
+// returns the sum of the elements of an array.
 function sumOfArray(arr) {
     return arr.reduce((partialSum, a) => partialSum + a, 0);
 }
 
+// this is used to maximum sum possible of a contiguous subarray of the passed array
 function maxSumOfSubArray(arr) {
     let arrLen = arr.length;
     let maxSum = 0;
 
+    // easy conditions if array length is less than or equal to 2
     if(arrLen <= 0) {
         maxSum = -1;
     } else if(arrLen == 1) {
@@ -13,6 +16,7 @@ function maxSumOfSubArray(arr) {
     } else if(arrLen == 2) {
         maxSum = Math.max(arr[0], arr[1]);
     } else {
+        // initiate the minimum and maxim
         let MinLen = 2;
         let MaxLen = arrLen - 1;
 
@@ -37,7 +41,7 @@ const input2 = [1, 3, 10, 4, -1, 1, 8];
 const input3 = [1, 2, 3, 4, -10];
 const input4 = [100, 35];
 
-console.log('maxSumOfSubArray(input1) =', maxSumOfSubArray(input1));
-console.log('maxSumOfSubArray(input2) =', maxSumOfSubArray(input2));
-console.log('maxSumOfSubArray(input3) =', maxSumOfSubArray(input3));
-console.log('maxSumOfSubArray(input4) =', maxSumOfSubArray(input4));
+console.log('Maximum possible sum of input1 =', maxSumOfSubArray(input1));
+console.log('Maximum possible sum of input2 =', maxSumOfSubArray(input2));
+console.log('Maximum possible sum of input3 =', maxSumOfSubArray(input3));
+console.log('Maximum possible sum of input4 =', maxSumOfSubArray(input4));
