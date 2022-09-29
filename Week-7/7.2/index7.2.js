d41bde2771f7f51a9dc9316c91c6ec872de35feb
mr-hungama <mihir.rego@hungama.com>
2022-09-29 10:55:22 +0530

    added rotate SLL
    added function to rotate SLL k times

--- Week-7/7.2/index7.2.js
+++ Week-7/7.2/index7.2.js
@@ -30,10 +30,33 @@ function traverseSLL(head) {
 }
 
 function rotateSLL(head, k) {
+    let lenSLL = 1;
+    let tail = head;
+
+    while(tail.next !== null) {
+        tail = tail.next;
+        lenSLL++;
+    }
+
+    console.log('length SLL:', lenSLL);
     
+    k %= lenSLL;
+    if(k === 0) return head;
+
+    tail.next = head;
+
+    while(k--) {
+        tail = tail.next;
+    }
+
+    head = tail.next;
+    tail.next = null;
+    return head;
 }
 
 let array1 = [2,4,6,8,10];
 let head1 = createSLL(array1);
 
+traverseSLL(head1);
+head1 = rotateSLL(head1, 4);
 traverseSLL(head1);
