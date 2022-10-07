68b59ed81bb4ac87885109a9b0ad37f057ddc3fd
mr-hungama <mihir.rego@hungama.com>
2022-10-07 16:57:35 +0530

    complete 7.6
    assignment completed

--- Week-7/7.6/index7.6.js
+++ Week-7/7.6/index7.6.js
@@ -3,10 +3,10 @@ let stack2 = [];
 
 // adding an element to queue
 function enqueue(elem) {
-    while(stack2.length > 0) {
-        let el = stack2.pop();
-        stack1.push(el);
-    }
+    // while(stack2.length > 0) {
+    //     let el = stack2.pop();
+    //     stack1.push(el);
+    // }
 
     stack1.push(elem);
     printQueue();
@@ -17,8 +17,10 @@ function dequeue() {
     if(stack1.length === 0) console.log('Queue is empty!');
     else {
         // move all elements from s1 to s2
+        // console.log('hello stack1:', stack1);
         while(stack1.length > 0) stack2.push(stack1.pop());
 
+        // console.log('hello stack2:', stack2);
         let deq = stack2.pop();
         console.log('popped element:', deq);
 
@@ -36,13 +38,47 @@ function printQueue() {
     console.log('current queue:', queue);
 }
 
-enqueue('a');
-enqueue('b');
-enqueue('c');
-dequeue();
-dequeue();
-enqueue('d');
-dequeue();
-enqueue('e');
-dequeue();
+
+// process the set of incoming instructions as per the array arr passed
+function processInput(arr) {
+    console.log('PROCESSING INPUT: ', arr);
+
+    stack1 = [];
+    stack2 = [];
+
+    while(arr.length > 0) {
+        let order = arr.shift();
+        // console.log('order:', order);
+
+        if(order === 1) {
+            let elem = arr.shift();
+            // console.log('enqueue:', elem);
+            enqueue(elem);
+        } else if(order === 2) {
+            // console.log('dequeue');
+            dequeue();
+        }
+    }
+
+    console.log('PROCESSING COMPLETE!');
+}
+
+
+const instructionSet1 = [1, 2, 1, 3, 2, 1, 4, 2];
+const instructionSet2 = [1, 2, 2, 2, 1, 4];
+
+processInput(instructionSet1);
+console.log('---------------------------');
+processInput(instructionSet2);
+console.log('---------------------------');
+
+// enqueue('a');
+// enqueue('b');
+// enqueue('c');
+// dequeue();
+// dequeue();
+// enqueue('d');
+// dequeue();
+// enqueue('e');
+// dequeue();
 
