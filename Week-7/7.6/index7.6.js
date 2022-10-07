45d58646c7783f42705d74db8af4ea8a4aa9aac1
mr-hungama <mihir.rego@hungama.com>
2022-10-07 16:19:03 +0530

    update 7.6
    implemented dequeue method

--- Week-7/7.6/index7.6.js
+++ Week-7/7.6/index7.6.js
@@ -14,7 +14,19 @@ function enqueue(elem) {
 
 // remove first element from the queue
 function dequeue() {
+    if(stack1.length === 0) console.log('Queue is empty!');
+    else {
+        // move all elements from s1 to s2
+        while(stack1.length > 0) stack2.push(stack1.pop());
 
+        let deq = stack2.pop();
+        console.log('popped element:', deq);
+
+        // move all elements from s2 back to s1
+        while(stack2.length > 0) stack1.push(stack2.pop());
+
+        printQueue();
+    }
 }
 
 // print current queue
@@ -27,3 +39,10 @@ function printQueue() {
 enqueue('a');
 enqueue('b');
 enqueue('c');
+dequeue();
+dequeue();
+enqueue('d');
+dequeue();
+enqueue('e');
+dequeue();
+
