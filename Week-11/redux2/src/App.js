a3f2871af3c69b66a8497d3b26e4dee9fccdd44f
mr-rego <mihir.rego@outlook.com>
2022-11-18 10:46:55 +0530

    Assigment complete

--- Week-11/redux2/src/App.js
+++ Week-11/redux2/src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, reset } from './redux/counterSlice';
 
 function App() {
+  const count = useSelector(state => state.counter.value);
+  const dispatch = useDispatch();
+
   return (
     <div className="App">
-
+      <p>You've walked {count} steps today!</p>
+      <input id="addstepbutton" type="button" value="Add a Step" onClick={() => dispatch(increment())} />
+      <input id="resetbutton" type="button" value="Reset Steps" onClick={() => dispatch(reset())} />
     </div>
   );
 }
---
+++ Week-11/redux2/src/redux/counterSlice.js
@@ -0,0 +1,20 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const counterSlice = createSlice({
+    name: "counter",
+    initialState: {
+        value: 0,
+    },
+    reducers: {
+        increment: (state) => {
+            state.value += 1;
+        },
+        reset: (state) => {
+            state.value = 0;
+        }
+    }
+});
+
+export const { increment, reset } = counterSlice.actions;
+
+export default counterSlice.reducer;
